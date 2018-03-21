651b3079a2926ce46156ee6d52515a53c4b10dc2
quangtran9kz <1451010148quang@ou.edu.vn>
2018-03-21 20:53:39 +0700

    Chỉnh Sửa tên thành viên, Thêm file Test phần Dịch Vụ

--- Quanlykhachsan/FormAbout.Designer.cs
+++ Quanlykhachsan/FormAbout.Designer.cs
@@ -75,9 +75,9 @@
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(71, 90);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(161, 20);
+            this.label3.Size = new System.Drawing.Size(164, 20);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Nguyễn Thị Minh Tâm";
+            this.label3.Text = "Phạm Ngọc Minh Tâm";
             // 
             // label4
             // 
